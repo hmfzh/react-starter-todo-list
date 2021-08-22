2da0ccfeea0c12cb45c2f7375e0d4b198b394e29
Muhammad Ilham Hafizha <75237342+hmfzh@users.noreply.github.com>
2021-08-22 11:50:56 +0700

    Membuat Pages

---
+++ src/component/TodoForm.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TodoFrom = () => {
+  return (
+    <section className="add">
+      <form className="add-form">
+        <input type="text" name="" className="add-input" />
+        <button className="add-btn main-black-color">Add</button>
+      </form>
+    </section>
+  );
+};
+
+export default TodoFrom;
---
+++ src/component/Todos.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Todo from "./Todo";
+
+const Todos = () => {
+  const todos = [
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar Props"
+    },
+    {
+      text: "Belajar Hooks"
+    },
+    {
+      text: "Belajar State"
+    },
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "Belajar Props"
+    },
+    {
+      text: "Belajar Hooks"
+    },
+    {
+      text: "Belajar State"
+    }
+  ];
+  return (
+    <section className="todos">
+      {todos.map((todo) => {
+        return <Todo text={todo.text} />;
+      })}
+    </section>
+  );
+};
+
+export default Todos;
